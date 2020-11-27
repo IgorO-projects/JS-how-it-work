556298a78bc821fb0af452eb7c0ed3f105957107
Igor <Travis789@gmail.com>
2020-11-27 02:14:37 +0100

    task-06 done via while & do while

--- js/cycle-dowhile.js
+++ js/cycle-dowhile.js
@@ -1,7 +1,59 @@
-let name = '';
+// let name = '';
 
-do {
-    name = prompt('Введите имя длиннее 3х символов', '');
-} while (name.length < 3);
+// do {
+//     name = prompt('Введите имя длиннее 3х символов', '');
+// } while (name.length < 3);
 
-console.log('Ввели имя: ', name);
+// console.log('Ввели имя: ', name);
+
+// ******************************************************************************************************************
+
+// let order;
+
+// do {
+//     order = prompt('Execute order 66');
+//     console.log(typeof order);
+//     order = Number(order);
+
+//     if (order === 66) {
+//         alert('Yes, my Lord!');
+//         break;
+//     } else if (order !== null) {
+//         alert('TRAITOR');
+//     } else {
+//         alert('DO IT!');
+//     }
+
+// } while (order !== 66) 
+
+// ******************************************************************************************************************
+
+// HM-01-JS
+
+// let total = 0;
+
+// do {
+//     let input = prompt('видете число');
+//     console.log('тип значения из input:', typeof input);
+//     console.log('является не числом значение из input:', isNaN(Number(input)));
+
+//     if(input === null) {
+//         break;
+//     }
+
+//     input = Number(input);
+
+//     const notANumber = Number.isNaN(input);
+
+//     if(notANumber) {
+//         alert('Было введено не число, попробуйте еще раз');
+//         continue;
+//     }
+
+//     total += input;
+//     console.log(total);
+
+// } while (true)
+// total = alert(`Общая сумма чисел равна ${total}`);
+
+// ******************************************************************************************************************
--- js/cycle-while.js
+++ js/cycle-while.js
@@ -2,13 +2,14 @@ let input;
 let total = 0; 
 
 while(input !== null) {
-    input = prompt('видете число');
-    console.log(isNaN(Number(input)));
+    input = prompt('ведете число');
+    console.log('тип значения из input:', typeof input);
+    console.log('является не числом значение из input:', isNaN(Number(input)));
 
     // Мы спрашиваем у input, который переведен уже в число, 
     // является ли он не числом? и получаем ответ true. 
     // При этом условие говорит, если input не число тогда выполнил alert, 
-    // а если input число, сделай что написано  в иначе else.
+    // а если input число, сделай что написано в "иначе" else.
     if(isNaN(Number(input)))  {
         alert('Было введено не число, попробуйте еще раз');
     } else {
@@ -20,3 +21,4 @@ while(input !== null) {
         }
     } 
 }
+
